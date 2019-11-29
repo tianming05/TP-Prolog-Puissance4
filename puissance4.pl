f776405bfe890319f9dae8e32cef7ddec333b653
Emmylrd <36962728+Emmylrd@users.noreply.github.com>
2019-11-29 11:55:12 +0100

    Ajout condition sur entrée

--- puissance4.pl
+++ puissance4.pl
@@ -1,27 +1,37 @@
 :- dynamic board/1.
 
+%Méthodes générales
 incr(X, X1) :- X1 is X+1.
 decr(X,X1):-X1 is X-1.
 
 %Renvoie l'index d'une case à partir de la ligne L et de la colonne C.
-indexCase(C,L,Index):- Index is C+L*7.
+indexCase(C,L,Index):- Index is C+(5-L)*7.
 
 %Défini si une case est vide : renvoie true si X est vide
 caseLibre(X, Board):- nth0(X,Board,Val), var(Val).
 
+%Vérifie si la Colonne est valide:
+%1-  0<Colonne<6
+%2-  La Colonne n'est pas remplie
+
+possible(Colonne,Board):- Colonne@=<6, Colonne@>=0,indexCase(Colonne,5,Index),caseLibre(Index,Board).
+
 %Trouve a quel endroit placer le pion en fonction de la colonne.
 % retourne l'indice de la premiere case vide de la colonne
-
 calculPosition(Colonne,LigneActuelle,LigneActuelle,Board):- indexCase(Colonne,LigneActuelle,Index), caseLibre(Index,Board),!.
-calculPosition(Colonne,LigneActuelle,BonneLigne,Board):- decr(LigneActuelle,LignePrecedente), calculPosition(Colonne,LignePrecedente,BonneLigne,Board).
+calculPosition(Colonne,LigneActuelle,BonneLigne,Board):- incr(LigneActuelle,LignePrecedente), calculPosition(Colonne,LignePrecedente,BonneLigne,Board).
+
+%Jouer un pion a l'emplacement Move
 playMove(Board,Move,NewBoard,Player) :- Board=NewBoard, nth0(Move,NewBoard,Player).
+%Actualiser le plateau
 applyIt(Board,NewBoard) :- retract(board(Board)), assert(board(NewBoard)).
 
 play():-
  board(Board), % instanciate the board from the knowledge base
  displayBoard, % print it
  read(Colonne),
- calculPosition(Colonne,5,BonneLigne,Board),
+ possible(Colonne,Board),
+ calculPosition(Colonne,0,BonneLigne,Board),
  indexCase(Colonne,BonneLigne,Index),
  playMove(Board,Index,NewBoard,'x'),
  applyIt(Board,NewBoard),
@@ -38,11 +48,11 @@ printVal(N) :- board(B), nth0(N,B,Val), write(Val).
 displayBoard:-
  writeln('*-------------*'),
  printVal(0),write('|'), printVal(1),write('|'), printVal(2),write('|'),printVal(3),write('|'), printVal(4),write('|'),printVal(5),write('|'), printVal(6), writeln('|'),
-  printVal(7),write('|'), printVal(8),write('|'), printVal(9),write('|'),printVal(10),write('|'), printVal(11),write('|'), printVal(12),write('|'), printVal(13), writeln('|'),
-   printVal(14),write('|'), printVal(15),write('|'), printVal(16),write('|'),printVal(17),write('|'), printVal(18),write('|'),printVal(19),write('|'), printVal(20), writeln('|'),
-   printVal(21),write('|'), printVal(22),write('|'), printVal(23),write('|'),printVal(24),write('|'), printVal(25),write('|'),printVal(26),write('|'), printVal(27), writeln('|'),
-   printVal(28),write('|'), printVal(29),write('|'), printVal(30),write('|'),printVal(31),write('|'), printVal(32),write('|'),printVal(33),write('|'), printVal(34), writeln('|'),
-   printVal(35),write('|'), printVal(36),write('|'), printVal(37),write('|'),printVal(38),write('|'), printVal(39),write('|'),printVal(40),write('|'), printVal(41), writeln('|'),
+ printVal(7),write('|'), printVal(8),write('|'), printVal(9),write('|'),printVal(10),write('|'), printVal(11),write('|'), printVal(12),write('|'), printVal(13), writeln('|'),
+ printVal(14),write('|'), printVal(15),write('|'), printVal(16),write('|'),printVal(17),write('|'), printVal(18),write('|'),printVal(19),write('|'), printVal(20), writeln('|'),
+ printVal(21),write('|'), printVal(22),write('|'), printVal(23),write('|'),printVal(24),write('|'), printVal(25),write('|'),printVal(26),write('|'), printVal(27), writeln('|'),
+ printVal(28),write('|'), printVal(29),write('|'), printVal(30),write('|'),printVal(31),write('|'), printVal(32),write('|'),printVal(33),write('|'), printVal(34), writeln('|'),
+ printVal(35),write('|'), printVal(36),write('|'), printVal(37),write('|'),printVal(38),write('|'), printVal(39),write('|'),printVal(40),write('|'), printVal(41), writeln('|'),
 
  writeln('*-------------*').
 
